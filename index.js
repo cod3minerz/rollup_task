4e004ddda8e15dc0d075503706d160178ed15a29
Hashpix <kirill.krivonos06@gmail.com>
2025-09-18 14:39:38 +0300

    task 2 rollup
    task 2 rollup

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+import './index.css'
+import jsLogo from './assets/jslogo.png'
+
+const text = document.createElement('h1')
+text.textContent ='I love JavaScript'
+document.body.append(text)
+
+const pic = document.createElement('img')
+pic.src = 'assets/jslogo.png'
+document.body.append(pic)
+
+const array = [1,2,3].map(i => i+10)
+console.log(array)
